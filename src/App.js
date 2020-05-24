521682e571985637c517aea640728e3e5474f633
Kachi123 <kachi.erugo@codex.com.ng>
2020-05-24 13:10:46 +0100

    shop page created

--- src/App.js
+++ src/App.js
@@ -6,19 +6,15 @@ import './App.css';
 
 
 import HomePage from './pages/Homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component'
 
-const HatsPage = () => (
-  <div>
-   <h1>Hats Page</h1>
-  </div>
-);
 
 function App() {
   return (
     <div>
       <Switch>
           <Route exact path='/' component={HomePage} />
-          <Route  path='/hats' component={HatsPage} />
+          <Route  path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
