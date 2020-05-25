94419ceaf8c7e6c32bcb40ac0acecaf88d44c261
Kachi123 <kachi.erugo@codex.com.ng>
2020-05-25 04:14:15 +0100

    addition of custom-button, form-input, sign-in, sign-in-and-sign-up

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import './App.css';
 import HomePage from './pages/Homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component'
 import Header from './components/header/header.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component'
 
 
 function App() {
@@ -17,6 +18,7 @@ function App() {
       <Switch>
           <Route exact path='/' component={HomePage} />
           <Route  path='/shop' component={ShopPage} />
+          <Route path='/signin' component={SignInAndSignUpPage}/>
       </Switch>
     </div>
   );
